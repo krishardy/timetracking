ac52bf12549069f3371b90964398a28122481cfa
Kris Hardy <kris@rkrishardy.com>
2021-09-23 10:12:50 +1200

    Adding GPL v3 license

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-extern crate clap;
+/*
+Timetracking generates reports from timesheet files
+Copyright (C) 2021  Kris Hardy
+
+Timetracking is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Timetracking is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Timetracking.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 use clap::{App, Arg};
 use log::{debug, error, info, trace, warn};
@@ -18,7 +34,7 @@ const GITVERSION: &str = git_version!();
 fn main() {
     let matches = App::new(APPNAME)
         .version(format!("{} ({})", VERSION, GITVERSION).as_str())
-        .author("Kris Hardy <hardyrk@gmail.com>")
+        .author("Copyright (C) 2021  Kris Hardy <hardyrk@gmail.com>")
         .about("Generates reports from timetracking CSV files")
         .arg(
             Arg::with_name("verbose")
